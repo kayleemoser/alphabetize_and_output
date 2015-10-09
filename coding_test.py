10504dabaf9ce591aed59874cf6a2982f7e22514
Kaylee Moser <kay.moser@me.com>
2015-10-09 13:16:11 -0700

    first code commit

---
+++ coding_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# Coding project for ice.com
+# Kaylee Moser
+# October 9, 2015
+# Python version 2.7.10
+
+import sys
+
+# place given arguments into list
+sentence = sys.argv
+sentence.pop(0)
+
+print sentence
