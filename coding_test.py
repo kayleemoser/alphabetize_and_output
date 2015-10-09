863d46ae3dedb9f7012c77072d2e57c7409ed33e
Kaylee Moser <kay.moser@me.com>
2015-10-09 14:34:56 -0700

    sorts into columns for output

--- coding_test.py
+++ coding_test.py
@@ -11,4 +11,23 @@ import sys
 sentence = sys.argv
 sentence.pop(0)
 
+# sort alphabetically
+sentence.sort()
+
+# initialize columns for output
+col_a = []
+col_b = []
+col_c = []
+
+sentence_len = len(sentence)
+col_a_len = col_b_len = col_c_len = sentence_len/3
+col_rem = sentence_len%3
+
+if (col_rem >= 1):
+    col_a_len += 1
+if (col_rem >= 2):
+    col_b_len += 1
+
+
+
 print sentence
