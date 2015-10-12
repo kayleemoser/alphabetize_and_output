2019ae5b61b731b9be9a17e69763c3a9527016d2
Kaylee Moser <kay.moser@me.com>
2015-10-12 10:53:35 -0700

    working program; still need stdin and ljust fix

--- coding_test.py
+++ coding_test.py
@@ -8,26 +8,39 @@
 import sys
 
 # place given arguments into list
-sentence = sys.argv
-sentence.pop(0)
+sentence = sys.argv[1:]
 
 # sort alphabetically
 sentence.sort()
 
-# initialize columns for output
-col_a = []
-col_b = []
-col_c = []
-
-sentence_len = len(sentence)
-col_a_len = col_b_len = col_c_len = sentence_len/3
-col_rem = sentence_len%3
-
-if (col_rem >= 1):
+# find column lengths
+col_a_len = col_b_len = col_c_len = len(sentence)/3
+remainder = len(sentence)%3
+if (remainder >= 1):
     col_a_len += 1
-if (col_rem >= 2):
+if (remainder >= 2):
     col_b_len += 1
 
+# split into lists per column
+col_a = sentence[:col_a_len]
+col_b = sentence[col_a_len:col_a_len+col_b_len]
+col_c = sentence[col_a_len+col_b_len:]
+
+# create string to print per row
+for i in range(0,col_a_len):
+    # column a will always be longest
+    retStr = col_a[i].ljust(10)
+    
+    if (i < col_b_len):
+      retStr += col_b[i].ljust(10)
+    if (i < col_c_len):
+      retStr += col_c[i].ljust(10)
+
+    print retStr
 
 
-print sentence
+
+
+
+
+
