a98cecf2bf3d80589ac2f7bf55a1837b6128f2ac
Kaylee Moser <kay.moser@me.com>
2015-10-12 11:12:07 -0700

    left justification corrected

--- coding_test.py
+++ coding_test.py
@@ -26,15 +26,25 @@ col_a = sentence[:col_a_len]
 col_b = sentence[col_a_len:col_a_len+col_b_len]
 col_c = sentence[col_a_len+col_b_len:]
 
+def longest_word(column):
+    word = 0
+    for i in range(0,len(column)):
+        if (len(column[i]) > word):
+            word = len(column[i])
+    return word
+
+longest_a = longest_word(col_a)
+longest_b = longest_word(col_b)
+longest_c = longest_word(col_c)
+
 # create string to print per row
 for i in range(0,col_a_len):
     # column a will always be longest
-    retStr = col_a[i].ljust(10)
-    
+    retStr = col_a[i].ljust(longest_a+1)
     if (i < col_b_len):
-      retStr += col_b[i].ljust(10)
+      retStr += col_b[i].ljust(longest_b+1)
     if (i < col_c_len):
-      retStr += col_c[i].ljust(10)
+      retStr += col_c[i].ljust(longest_c+1)
 
     print retStr
 
