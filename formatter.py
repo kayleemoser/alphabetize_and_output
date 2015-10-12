fdc3f57d95981aca5f6053b8ba97b8fa6b261b30
Kaylee Moser <kay.moser@me.com>
2015-10-12 12:42:10 -0700

    added main

--- formatter.py
+++ formatter.py
@@ -16,40 +16,43 @@ def longest_word(column):
             word_len = len(column[i])
     return word_len
 
-# Place given arguments into list and sort alphabetically.
-sentence = sys.stdin.readline().split()
-sentence.sort()
+def main():
+    # Place given arguments into list and sort alphabetically.
+    sentence = sys.stdin.readline().split()
+    sentence.sort()
+    
+    # Find column lengths.
+    col_a_len = col_b_len = col_c_len = len(sentence)/3
+    remainder = len(sentence)%3
+    if (remainder >= 1):
+        col_a_len += 1
+    if (remainder >= 2):
+        col_b_len += 1
+    
+    # Split into separate lists per column.
+    col_a = sentence[:col_a_len]
+    col_b = sentence[col_a_len:col_a_len+col_b_len]
+    col_c = sentence[col_a_len+col_b_len:]
+    
+    # Find the longest word for each column to use when
+    # formatting below
+    longest_a = longest_word(col_a)
+    longest_b = longest_word(col_b)
+    longest_c = longest_word(col_c)
+    
+    # Create string to print per row.
+    for i in range(0,col_a_len):
+        # Column A will always be longest.
+        ret_str = col_a[i].ljust(longest_a+1)
+        if (i < col_b_len):
+          ret_str += col_b[i].ljust(longest_b+1)
+        if (i < col_c_len):
+          ret_str += col_c[i].ljust(longest_c+1)
+    
+        print ret_str
 
-# Find column lengths.
-col_a_len = col_b_len = col_c_len = len(sentence)/3
-remainder = len(sentence)%3
-if (remainder >= 1):
-    col_a_len += 1
-if (remainder >= 2):
-    col_b_len += 1
-
-# Split into separate lists per column.
-col_a = sentence[:col_a_len]
-col_b = sentence[col_a_len:col_a_len+col_b_len]
-col_c = sentence[col_a_len+col_b_len:]
-
-# Find the longest word for each column to use when
-# formatting below
-longest_a = longest_word(col_a)
-longest_b = longest_word(col_b)
-longest_c = longest_word(col_c)
-
-# Create string to print per row.
-for i in range(0,col_a_len):
-    # Column A will always be longest.
-    ret_str = col_a[i].ljust(longest_a+1)
-    if (i < col_b_len):
-      ret_str += col_b[i].ljust(longest_b+1)
-    if (i < col_c_len):
-      ret_str += col_c[i].ljust(longest_c+1)
-
-    print ret_str
-
+if (__name__ == '__main__'):
+    main()
 
 
 
